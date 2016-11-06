ef9a0169769fa1aa71e9666d07ce5217d0527b7b
Drew Williams <williams.r.drew@gmail.com>
2016-11-05 21:15:29 -0400

    made kangaroo pong

---
+++ src/kangaroo.py
@@ -0,0 +1,36 @@
+import pygame
+from utilities import load_png
+
+class kangaroo(pygame.sprite.Sprite):
+    
+    def __init__(self, side):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png('/home/drew/development/kangaroogame/KangarooGame/assets/img/Kangaroo.png')
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.side = side
+        self.speed = 10
+        self.state = "still"
+        self.reinit()
+
+    def reinit(self):
+        self.state = "still"
+        self.movepos = [0,0]
+        if self.side == "left":
+            self.rect.midleft = self.area.midleft
+        elif self.side == "right":
+            self.rect.midright = self.area.midright
+
+    def update(self):
+        newpos = self.rect.move(self.movepos)
+        if self.area.contains(newpos):
+            self.rect = newpos
+        pygame.event.pump()
+
+    def moveup(self):
+        self.movepos[1] = self.movepos[1] - (self.speed)
+        self.state = "moveup"
+
+    def movedown(self):
+        self.movepos[1] = self.movepos[1] + (self.speed)
+        self.state = "movedown"

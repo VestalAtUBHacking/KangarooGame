4b9d793ad34fbcf0fcc16e6fe51cb83c425ef147
Sean Kunz <skunz1@binghamton.edu>
2016-11-05 20:21:02 -0400

    added kangaroo

--- src/__init__.py
+++ src/__init__.py
@@ -4,8 +4,13 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 done = False
 
+kang = pygame.image.load("kangaroo.gif")
+kangrect = kang.get_rect()
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     pygame.display.flip()
+
+    screen.blit(kang, kangrect)
